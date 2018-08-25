5538ebefb39d7719734d9cd4fd0ffd1d7e770f57
James Minton <james-minton@github.com>
2018-08-25 17:20:59 -0400

    Add smooth scroll, remove rss

--- site/themes/hugo-strata-theme/static/js/jm.js
+++ site/themes/hugo-strata-theme/static/js/jm.js
@@ -21,3 +21,20 @@
              $('.listening-to2').append(html2);
         });
 });
+
+// Smooth scroll
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 900);
+            return false;
+        }
+    }
+});
